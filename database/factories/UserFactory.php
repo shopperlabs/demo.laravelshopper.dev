fbf63ecab78bfc2086e1a6d7a7a596e8082636c3
Arthur Monney <monneylobe@gmail.com>
2023-08-23 01:10:00 +0200

    Update project

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,15 +12,11 @@ use Illuminate\Support\Str;
  */
 final class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
+    public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->name(),
+            'last_name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
@@ -28,12 +24,7 @@ final class UserFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
+    public function unverified(): UserFactory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
