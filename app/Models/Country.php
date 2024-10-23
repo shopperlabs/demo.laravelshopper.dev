c4e0982d59024f7f0c5072c36d20eaab18902d14
Arthur Monney <monneylobe@gmail.com>
2024-10-23 02:44:16 +0200

    feat: (SHO-10) update homepage, tests and add new components (#15)
    * feat: (SHO-10) update homepage, tests and add new components
    
    * chore: apply laravel pint

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -7,7 +7,6 @@ namespace App\Models;
 use Database\Factories\CountryFactory;
 use Shopper\Core\Models\Country as Model;
 
-// @ToDo: After update shopper delete this file
 final class Country extends Model
 {
     protected static function newFactory(): CountryFactory
---
+++ resources/views/includes/_decorative_images.blade.php
@@ -0,0 +1,27 @@
+<div aria-hidden="true" class="hidden pointer-events-none lg:block lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-8xl">
+    <div class="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+        <div class="flex items-center space-x-6 lg:space-x-8">
+            <div class="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                <div class="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
+                    <img src="{{ asset('images/hero/cover_4.jpg') }}" alt="" class="h-full w-full object-cover object-center">
+                </div>
+            </div>
+            <div class="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                <div class="h-64 w-44 overflow-hidden rounded-lg">
+                    <img src="{{ asset('images/hero/cover_5.jpg') }}" alt="" class="h-full w-full object-cover object-center">
+                </div>
+                <div class="h-64 w-44 overflow-hidden rounded-lg">
+                    <img src="{{ asset('images/hero/cover_6.jpg') }}" alt="" class="h-full w-full object-cover object-center">
+                </div>
+            </div>
+            <div class="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                <div class="h-64 w-44 overflow-hidden rounded-lg">
+                    <img src="{{ asset('images/hero/cover_7.jpg') }}" alt="" class="h-full w-full object-cover object-center">
+                </div>
+                <div class="h-64 w-44 overflow-hidden rounded-lg">
+                    <img src="{{ asset('images/hero/cover_8.jpg') }}" alt="" class="h-full w-full object-cover object-center">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
