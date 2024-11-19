f92c1abe1abaf699488cf7cd32b5418e1ce1f63f
Stevy Endaman <stevyabessolo@gmail.com>
2024-11-19 02:33:37 +0100

    fix:(SHO-58) fixing lint and phpstan errors

--- app/Livewire/Pages/Category/CategoryProducts.php
+++ app/Livewire/Pages/Category/CategoryProducts.php
@@ -1,24 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Livewire\Pages\Category;
 
+use App\Models\Product;
 use Livewire\Component;
 use App\Models\Category;
+use Illuminate\Support\Collection;
 use Illuminate\Contracts\View\View;
 
 final class CategoryProducts extends Component
 {
-    public $categorySlug;
-    public $category;
-    public $products = [];
+    public string $categorySlug;
 
-    public function mount($slug)
+    public Category $category;
+
+    public Collection  $products;
+
+    public function mount(string $slug): void
     {
         $this->categorySlug = $slug;
         $this->category = Category::where('slug', $this->categorySlug)->firstOrFail();
-        $this->products = $this->category->products; 
+        $this->products = $this->category->products;
     }
-    
+
     public function render(): View
     {
         return view('livewire.category.category-products');
--- tests/Feature/Livewire/Pages/Category/CategoryProductsTest.php
+++ tests/Feature/Livewire/Pages/Category/CategoryProductsTest.php
@@ -2,13 +2,12 @@
 
 declare(strict_types=1);
 
-use Livewire\Livewire;
-use App\Models\Product;
+use App\Livewire\Pages\Category\CategoryProducts;
 use App\Models\Category;
+use App\Models\Product;
+use Livewire\Livewire;
 
 use function Pest\Laravel\get;
-use App\Livewire\Pages\Category\CategoryProducts;
-
 
 describe(CategoryProducts::class, function (): void {
     it('can render category products page', function (): void {
@@ -27,13 +26,13 @@ describe(CategoryProducts::class, function (): void {
         $category = Category::factory()->create([
             'slug' => 'homme',
         ]);
-    
+
         $products = Product::factory()->count(3)->create();
-    
+
         foreach ($products as $product) {
             $product->categories()->attach($category->id);
         }
-    
+
         Livewire::test(CategoryProducts::class, ['slug' => $category->slug])
             ->assertSuccessful()
             ->assertSee($products[0]->name)
