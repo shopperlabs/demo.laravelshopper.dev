33ee7c47869c391103e7a9ed2344f88d98c983b9
Chri$ <samorytakougne@gmail.com>
2024-11-24 19:50:34 +0100

    feat:[SHO-59] add filter test for store page

--- app/Livewire/Pages/Store.php
+++ app/Livewire/Pages/Store.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Livewire\Pages;
 
 use App\Models\Product;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 use Illuminate\View\View;
 use Livewire\Component;
@@ -17,34 +18,33 @@ class Store extends Component
 
     public Collection $atributes;
 
-    private $products; // @phpstan-ignore-line
-
     /** @var string[] */
     public array $selectedAttributes = [];
 
     public function mount(): void
     {
-        $this->filterProducts();
         $this->atributes = Attribute::with('values')->where('is_enabled', true)->get();
+        $this->filterProducts();
     }
 
-    public function filterProducts(): void
+    public function filterProducts(): LengthAwarePaginator
     {
-        $this->products = Product::with(['media'])->parent();
+        $query = Product::with(['media', 'attributes'])->parent();
         if (! empty($this->selectedAttributes)) {
             foreach ($this->selectedAttributes as $attributeId => $valueIds) {
                 $selectedValueIds = array_keys(array_filter(array_map(function ($value) {
                     return $value;
                 }, $valueIds))); // @phpstan-ignore-line
                 if (! empty($selectedValueIds)) {
-                    $this->products = $this->products->whereHas('attributes', function ($query) use ($attributeId, $selectedValueIds) {
+                    $query = $query->whereHas('attributes', function ($query) use ($attributeId, $selectedValueIds) {
                         $query->where('attribute_id', $attributeId)
                             ->whereIn('attribute_value_id', $selectedValueIds);
                     });
                 }
             }
         }
-        $this->products = $this->products->paginate(12);
+
+        return $query->paginate(12);
     }
 
     public function updatedSelectedAttributes(): void
@@ -54,6 +54,6 @@ class Store extends Component
 
     public function render(): View
     {
-        return view('livewire.pages.store', ['products' => $this->products]);
+        return view('livewire.pages.store', ['products' => $this->filterProducts()]);
     }
 }
--- tests/Feature/Livewire/Pages/StoreTest.php
+++ tests/Feature/Livewire/Pages/StoreTest.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Livewire\Pages\Home;
 use App\Models\Product;
 use Livewire\Livewire;
 use Shopper\Core\Models\Attribute;
@@ -10,7 +9,7 @@ use Shopper\Core\Models\AttributeValue;
 
 use function Pest\Laravel\get;
 
-describe(Home::class, function (): void {
+describe(\App\Livewire\Pages\Store::class, function (): void {
     it('can render store page', function (): void {
         get(route('store.products'))->assertOk();
 
@@ -22,29 +21,28 @@ describe(Home::class, function (): void {
         $colorAttribute = Attribute::create(['name' => 'Color', 'slug' => 'color', 'type' => 'checkbox']);
         $redAttributeValue = AttributeValue::create(['value' => 'Red', 'key' => '#1e3a8a', 'attribute_id' => $colorAttribute->id]);
         $blueAttributeValue = AttributeValue::create(['value' => 'Blue', 'key' => '#ddf048', 'attribute_id' => $colorAttribute->id]);
-
         $sizeAttribute = Attribute::create(['name' => 'Size', 'slug' => 'size', 'type' => 'checkbox']);
         $smallAttributeValue = AttributeValue::create(['value' => 'Small', 'key' => 'XL', 'attribute_id' => $sizeAttribute->id]);
         $largeAttributeValue = AttributeValue::create(['value' => 'Large', 'key' => 'L', 'attribute_id' => $sizeAttribute->id]);
+        $product1 = Product::factory()->create(['name' => 'Red Small Shirt', 'slug' => 'red-shirt']);
+        $product2 = Product::factory()->create(['name' => 'Blue Large Shirt', 'slug' => 'blue-shirt']);
 
-        $product1 = Product::create(['name' => 'Red Small Shirt', 'slug' => 'red-shirt']);
-        $product1->attributes()->createMany([$colorAttribute->id => ['attribute_value_id' => $redAttributeValue->id, 'attribute_id' => $colorAttribute->id],
-            $sizeAttribute->id => ['attribute_value_id' => $smallAttributeValue->id, 'attribute_id' => $colorAttribute->id]]);
+        $product1->attributes()->createMany([
+            ['attribute_value_id' => $redAttributeValue->id, 'attribute_id' => $colorAttribute->id],
+            ['attribute_value_id' => $smallAttributeValue->id, 'attribute_id' => $sizeAttribute->id],
+        ]);
 
-        $product2 = Product::create(['name' => 'Blue Large Shirt', 'slug' => 'blue-shirt']);
-        $product2->attributes()->createMany([$colorAttribute->id => ['attribute_value_id' => $blueAttributeValue->id, 'attribute_id' => $colorAttribute->id],
-            $sizeAttribute->id => ['attribute_value_id' => $largeAttributeValue->id, 'attribute_id' => $colorAttribute->id]]);
+        $product2->attributes()->createMany([
+            ['attribute_value_id' => $blueAttributeValue->id, 'attribute_id' => $colorAttribute->id],
+            ['attribute_value_id' => $largeAttributeValue->id, 'attribute_id' => $sizeAttribute->id],
+        ]);
 
-        $component = Livewire::test(\App\Livewire\Pages\Store::class)
+        Livewire::test(\App\Livewire\Pages\Store::class)
             ->set('selectedAttributes', [
                 $sizeAttribute->id => [$smallAttributeValue->id => true],
             ])
             ->call('filterProducts')
-            ->assertViewHas('products', function ($products) use ($product1) {
-                return ! $products->contains($product1);
-            })
-            ->assertViewHas('products', function ($products) use ($product2) {
-                return ! $products->contains($product2);
-            });
+            ->assertSee(route('single-product', ['slug' => $product1->slug]))
+            ->assertDontSee(route('single-product', ['slug' => $product2->slug]));
     });
 });
